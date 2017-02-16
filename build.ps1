1ed274e14d867bfe373d41f5bb86b7361742ef90
Mark Kraus <git@markekraus.com>
2017-02-16 04:43:46 -0600

    CI/CD Prep

---
+++ build.ps1
@@ -0,0 +1,13 @@
+param ($Task = 'Default')
+
+# Grab nuget bits, install modules, set build variables, start build.
+Get-PackageProvider -Name NuGet -ForceBootstrap | Out-Null
+
+Install-Module Psake, PSDeploy, BuildHelpers -force
+Install-Module Pester -Force -SkipPublisherCheck
+Import-Module Psake, BuildHelpers
+
+Set-BuildEnvironment
+
+Invoke-psake -buildFile .\psake.ps1 -taskList $Task -nologo
+exit ( [int]( -not $psake.build_success ) )
